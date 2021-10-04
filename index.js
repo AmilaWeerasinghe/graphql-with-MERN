e77ab44cbba66a6127c13185dc554bd2f1d0efed
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-10-04 22:10:37 +0530

    connect to DB using mongoose and create mutaitons

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga')
+const mongoose = require('mongoose')
 
- 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
+
+const ToDo = new mongoose.Schema({
+  text: String,
+  complete: Boolean
+});
 const typeDefs = `
   type Query {
     hello(name: String): String!
+  }
+  type ToDo {
+    id : ID!
+    text : String!
+    complete : Boolean !
+
+  }
+  type Mutation{
+    createToDo(text: String!):ToDo
   }
 `
  
@@ -11,7 +30,18 @@ const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
   },
+  Mutation: {
+    createToDo: async (_,{text}) => {
+      const ToDo = new ToDo({text,complete:false});
+      await ToDo.save();
+      return ToDo;
+    }
+  }
 }
  
 const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+
+//once the mongoose database is connected we connect to the server
+mongoose.connection.once("open",function(){
+  server.start(() => console.log('Server is running on localhost:4000'))  
+})
